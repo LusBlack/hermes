33dbdcafa15c7142010147e77a8af51e64665f8c
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-10-30 23:42:18 +0100

    removed example routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
+//use App\Http\Controllers\ExampleController;
 //use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +15,7 @@ use App\Http\Controllers\ExampleController;
 |
 */
 Route::get('/', [UserController::class, "ShowCorrecthomepage"]);
-Route::get('/post', [ExampleController::class, "post"]);
+//Route::get('/post', [ExampleController::class, "post"]);
 Route::post('/register', [UserController::class,'register']);
 Route::post('/login', [UserController::class,'login']);
 Route::post('/logout', [UserController::class, 'logout']);
