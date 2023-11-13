2ae4d17f9ec3323c299bac9ca5f64cdf9eaca59f
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-13 02:09:18 +0100

    played around with more bootswatch

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,8 @@
     <title>Hermes</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
     {{-- <link rel="stylesheet" href="https://bootswatch.com/5/vapor/bootstrap.min.css"> --}}
-    <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css">
+    {{-- <link rel="stylesheet" href="https://bootswatch.com/5/cyborg/bootstrap.min.css"> --}}
+     <link rel= "stylesheet" href="https://bootswatch.com/5/morph/bootstrap.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
