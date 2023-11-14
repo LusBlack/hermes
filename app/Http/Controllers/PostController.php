a0ca2b9599f504cc79cef8b8112774df4269b980
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-14 03:01:16 +0100

    added markdown and securing user input

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -10,6 +11,7 @@ class PostController extends Controller
 
 
     public function viewSinglePost(Post $post) {
+     $post['body'] =  strip_tags(Str::markdown($post->body), '<p><ul<ol><li><strong><e,><h3><br>');
         return view('single-post', ['post'=> $post]);
     }
 
@@ -18,7 +20,7 @@ class PostController extends Controller
         'title'=> 'required',
         'body'=> 'required'
       ]);
-
+      //sanitizing user input
       $incomingFields['title'] = strip_tags($incomingFields['title']);
       $incomingFields['body'] = strip_tags($incomingFields['body']);
       $incomingFields['user_id'] = auth()->user()->id;
