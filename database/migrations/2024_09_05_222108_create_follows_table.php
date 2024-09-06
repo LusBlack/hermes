155d692df4b88a1c118559751e9f2820bff7b62b
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-09-06 21:49:18 +0100

    refactored follows table

--- database/migrations/2024_09_05_222108_create_follows_table.php
+++ database/migrations/2024_09_05_222108_create_follows_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained(); //follower
+            $table->unsignedBigInteger('followeduser');
+            $table->foreign('followeduser')->references('id')->on('users');
             $table->timestamps();
         });
     }
