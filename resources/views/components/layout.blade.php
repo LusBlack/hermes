39a82fc3c6dbad36d4353ee42008bde88ef41a5c
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-09-19 23:47:53 +0100

    improving page title display

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Hermes</title>
+    <title>@isset($doctitle)
+        {{$doctitle}} | Hermes
+        @else
+        Hermes
+        @endisset
+    </title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://bootswatch.com/5/quartz/bootstrap.min.css">
     {{-- <link rel="stylesheet" href="https://bootswatch.com/5/pulse/bootstrap.min.css"> --}}
