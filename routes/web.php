0f7e647c90f8c4c4dde867cbe1aa225b281eeca6
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-09-25 23:50:44 +0100

    added route for 'search' feature

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/post/{post}', [PostController::class,'viewSinglePost']);
 Route::delete('/post/{post}', [PostController::class, 'delete']);
 Route::get('/post/{post}/edit', [PostController::class, 'showEditForm']);
 Route::put('/post/{post}', [PostController::class, 'actuallyUpdate']);
+Route::get('/search/{term}', [PostController::class, 'search']);
 
 
 //profile related routes
