56ea54aa25a7fe228722f65247fcacea515ab7c6
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2024-09-27 02:38:40 +0100

    testing lister with logging

---
+++ app/Listeners/ExampleListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ExampleEvent;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ExampleListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     * @param \App\Events\ExampleEvent $event
+     * @return void
+     */
+    public function handle(ExampleEvent $event)
+    {
+        Log::debug("The user {$event->username} just performed {$event->action}");
+    }
+}
